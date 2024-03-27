fc2787725ea2effa4a27aaeccff237968aebbae2
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2024-03-27 09:51:48 -0300

    Adicionado material dos vídeos 3 e 4

---
+++ introducao-assembly/aritmetica/exemplo.asm
@@ -0,0 +1,4 @@
+	addi t1, zero, 3
+	addi t2, zero, 4
+	sub t3, t2, t1
+	sub t4, t1, t2
---
+++ introducao-assembly/memoria/test.asm
@@ -0,0 +1,6 @@
+	addi t0, zero, -1
+	addi sp, zero, -1
+	addi sp, sp, -4
+	sw sp, t0, 0
+	lb t0, sp, 0
+	lbu t0, sp, 0
